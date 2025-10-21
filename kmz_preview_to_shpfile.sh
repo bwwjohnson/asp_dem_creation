7500ade15a928dc2ee817d9f9dbedddf79756bcd
Ben Johnson <57494684+bwwjohnson@users.noreply.github.com>
2025-10-21 16:28:15 +0100

    Create kmz_preview_to_shpfile.sh

---
+++ kmz_preview_to_shpfile.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# script to extract polygons from PREVIEW_PHR1A_<NAME>.KMZ preview files for Pleiades imagery
+# B. Johnson 21-Oct-25
+
+# unzip kmz
+for f in *.KMZ; do unzip -o $f; done
+
+# tidy
+rm *.JPG
+rm *.png
+
+# convert kml to shapefile folder
+for f in *KML; do ogr2ogr -f "ESRI Shapefile" -skipfailures `basename $f .KML`.shp $f; done
+
+# remove spaces from AOI Produced.shp name
+for f in *.shp; do cd $f; rename 's/ /_/g' *; cd ..; done
+
+
+# move aoi polygon out of shapefile folder
+for f in *.shp; do
+aoi=`echo $f | awk -F"_" '{print $NF}' | awk -F"." '{print $1}'`
+for g in dbf prj shp shx; do mv $f/AOI*.$g ./$aoi.$g; done
+rm -r $f
+done
+
+rm *.KML
